81f012fb4243379acc44bc1c11cf5ae384aad6a2
Nikhil Katiyar <nikhilkatiyar0507@gmail.com>
2024-02-02 22:18:04 +0530

    handling URL

--- server/index.js
+++ server/index.js
@@ -1,15 +1,22 @@
 const http= require("http")
 const fs= require("fs")
+const url=require("url")
+
+
 
 const  myServer= http.createServer((req, res)=>{
+  if (req.url==="/favicon.ico") return res.end()
   const log =`${Date.now()}: ${req.url} new req received\n`
+  const myUrl=url.parse(req.url,true)
+  console.log(myUrl);
   fs.appendFile("log.txt", log, (err, data)=>{
-   switch(req.url){
+   switch(myUrl.pathname){
     case "/":
         res.end("HomePage")
         break;
     case "/about":
-      res.end("I am Nikhil katiyar")
+      const username = myUrl.query.myname
+      res.end(`hi, ${username}`)
       break
     default:
         res.end("404 not found")
