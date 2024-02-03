d3603b22e3dc89c902e9e0f8c60a55783c049088
Nikhil Katiyar <nikhilkatiyar0507@gmail.com>
2024-02-03 11:20:33 +0530

    http methods on server

--- server/index.js
+++ server/index.js
@@ -6,19 +6,26 @@ const url=require("url")
 
 const  myServer= http.createServer((req, res)=>{
   if (req.url==="/favicon.ico") return res.end()
-  const log =`${Date.now()}: ${req.url} new req received\n`
+  const log =`${Date.now()}:${req.method} ${req.url} new req received\n`
   const myUrl=url.parse(req.url,true)
-  console.log(myUrl);
+ 
   fs.appendFile("log.txt", log, (err, data)=>{
    switch(myUrl.pathname){
     case "/":
+      if (req.method==='GET') 
         res.end("HomePage")
         break;
     case "/about":
       const username = myUrl.query.myname
       res.end(`hi, ${username}`)
-      break
-    default:
+      break;
+      case "/signup":
+        if (req.method==='GET')
+        res.end('this is signup form')
+        else if (req.method==='POST')
+         // db query
+        res.end('Succes')
+      default:
         res.end("404 not found")
    }
   })
