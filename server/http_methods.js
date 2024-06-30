86d494a70fa5d2b6dfdd6fac2b75a98cecfe43f7
Nikhi Katiyar <nikhilkatiyar0507@gmail.com>
2024-06-30 18:00:43 +0530

    implementing http methods

---
+++ server/http_methods.js
@@ -0,0 +1,80 @@
+// const http= require("http")
+
+// // handling URLs in Nodejs
+
+// const  myServer = http.createServer((req,res)=>{
+//   if (req.url==="/favicon.ico") return res.end()
+//   const log =`${Date.now()}:${req.method} ${req.url} new req received\n`
+//   const myUrl=url.parse(req.url,true)
+ 
+//   fs.appendFile("log.txt", log, (err, data)=>{
+//    switch(myUrl.pathname){
+//     case "/":
+//       if (req.method==='GET') 
+//         res.end("HomePage")
+//         break;
+//     case "/about":
+//       const username = myUrl.query.myname
+//       res.end(`hi, ${username}`)
+//       break;
+//       case "/signup":
+//         if (req.method==='GET')
+//         res.end('this is signup form')
+//         else if (req.method==='POST')
+//          // db query
+//         res.end('Succes')
+//       default:
+//         res.end("404 not found")
+//    }
+//   })
+// },
+
+// myServer.listen(8000,()=>{
+//     console.log("server started");
+// }))
+
+
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+// handling URLs in Nodejs
+
+const myServer = http.createServer((req, res) => {
+  if (req.url === "/favicon.ico") return res.end();
+  
+  const log = `${Date.now()}:${req.method} ${req.url} new req received\n`;
+  const myUrl = url.parse(req.url, true);
+
+  fs.appendFile("log.txt", log, (err) => {
+    if (err) {
+      console.error("Error writing to log file:", err);
+      res.end("Server error");
+      return;
+    }
+    switch (myUrl.pathname) {
+      case "/":
+        if (req.method === 'GET') 
+          res.end("HomePage");
+        break;
+      case "/about":
+        const username = myUrl.query.myname;
+        res.end(`hi, ${username}`);
+        break;
+      case "/signup":
+        if (req.method === 'GET')
+          res.end('this is signup form');
+        else if (req.method === 'POST') {
+          // db query
+          res.end('Success');
+        }
+        break;
+      default:
+        res.end("404 not found");
+    }
+  });
+});
+
+myServer.listen(8000, () => {
+  console.log("server started");
+});
