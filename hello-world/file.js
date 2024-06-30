1e104ed5ea6d8c0761566030f7f5c48944959e06
Nikhi Katiyar <nikhilkatiyar0507@gmail.com>
2024-06-30 08:49:13 +0530

    append function added

--- hello-world/file.js
+++ hello-world/file.js
@@ -1,8 +1,8 @@
 
-const fs=require("fs")
+const fs=require("fs")  // fs module is used for file system and there are so many inbuilt functions.
 const os=require("os")    // to check thread pool size
 
-console.log(os.cpus().length);
+// console.log(os.cpus().length);
 // create or write file
 // //sync..  blocking req
 // fs.writeFileSync("./test.txt" ,"hey there")
@@ -14,10 +14,10 @@ console.log(os.cpus().length);
 
 
 // read the file
-// const result= fs.readFileSync("./contact.txt", "utf-8")      // return a result in variable
-// console.log(result);
+const result= fs.readFileSync("./contact.txt", "utf-8")      // return a result in variable
+console.log(result);
 
-// fs.readFile("./contact.txt", "utf-8", (err, result)=>{
+// fs.readFile("./contact.txt", "utf-8", (err, result)=>{   // does not return anything like  variable
 //     if(err){
 //         console.log("error", err);
 //     }
