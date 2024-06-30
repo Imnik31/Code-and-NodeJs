b0b03b6084c07eae8633efd5dc2d43f903e13c0b
Nikhi Katiyar <nikhilkatiyar0507@gmail.com>
2024-06-30 08:50:50 +0530

    important point added

--- hello-world/hello.js
+++ hello-world/hello.js
@@ -1,7 +1,17 @@
 // console.log("hey");
 // console.log("hey , I am Nikhil !");
 
-const math = require("./math")  // (add, sub instead of math -> called destructre)
+
+// window objects are not avilable in nodejs 
+
+// function add(a,b){
+//     return a+b;
+// }
+// console.log(add(3,1));
+
+const math = require("./math")  // (add, sub instead of math -> called destructre) and search in current dirctory
+// const math = require("fs") // it will search in own directory
 
 console.log(math.add(2,7));
 
+// npm init for node package manager.
