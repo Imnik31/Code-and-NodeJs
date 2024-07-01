f00790441bdf849d40935e983be64ab0381f6d31
Nikhi Katiyar <nikhilkatiyar0507@gmail.com>
2024-07-01 06:40:14 +0530

    http file updated with handler function

--- server/http_methods.js
+++ server/http_methods.js
@@ -35,18 +35,17 @@
 
 
 const http = require("http");
-const url = require("url");
 const fs = require("fs");
+const url = require("url");
 
 // handling URLs in Nodejs
-
-const myServer = http.createServer((req, res) => {
+function myHandler(req, res){
   if (req.url === "/favicon.ico") return res.end();
   
-  const log = `${Date.now()}:${req.method} ${req.url} new req received\n`;
+  const log = `${Date.now()}:${req.method} ${req.url} New req received\n`;
   const myUrl = url.parse(req.url, true);
 
-  fs.appendFile("log.txt", log, (err) => {
+  fs.appendFile("log.txt", log, (err, data) => {
     if (err) {
       console.error("Error writing to log file:", err);
       res.end("Server error");
@@ -73,7 +72,12 @@ const myServer = http.createServer((req, res) => {
         res.end("404 not found");
     }
   });
-});
+};
+
+
+  
+
+const myServer= http.createServer(myHandler);
 
 myServer.listen(8000, () => {
   console.log("server started");
